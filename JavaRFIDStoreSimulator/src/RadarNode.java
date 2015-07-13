15d7eb4a282ef0bf887dc35d8d8e04943be28c2d
Yusheng Ding <davidthinkleding@Gmail.com>
2015-07-13 02:03:48 -0500

    Add java project

---
+++ JavaRFIDStoreSimulator/src/RadarNode.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+
+
+public class RadarNode {
+	public HashMap<RadarNode,Double> link; //Double is reserved for future use---possibility
+	public double ReadSuccess = 0.95;										
+	public RadarNode() {
+		link = new HashMap<RadarNode,Double>();
+		System.out.println("node being created");
+	}
+	
+	public void addLink(RadarNode node,double possibility)//add link between 2 nodes
+	{
+		this.link.put(node, possibility);
+		node.link.put(this, possibility);
+	}
+}
+	
---
+++ JavaRFIDStoreSimulator/src/main.java
@@ -0,0 +1,53 @@
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//RadarNode node1 = new RadarNode();
+		RadarNode[] node = new RadarNode[9];  //10 Radars
+		//node 0
+		node[0].addLink(node[1], 0.25);
+		/*node[0].addLink(node[8], 0.25);
+		node[0].addLink(node[9], 0.25);
+		node[0].addLink(node[7], 0.25);
+		
+		//node 1
+		node[1].addLink(node[8], 0.25);
+		node[1].addLink(node[9], 0.25);
+		node[1].addLink(node[2], 0.25);
+		
+		//node 2
+		node[2].addLink(node[1], 0.30);
+		node[2].addLink(node[3], 0.30);
+		
+		//node 3
+		node[3].addLink(node[4], 0.25);
+		node[3].addLink(node[7], 0.25);
+		node[3].addLink(node[8], 0.25);
+		
+		//node 4
+		node[4].addLink(node[5], 0.30);
+		node[4].addLink(node[7], 0.30);
+		
+		//node 5
+		node[5].addLink(node[6], 0.25);
+		node[5].addLink(node[7], 0.25);
+		node[5].addLink(node[9], 0.25);
+		
+		//node 6
+		node[6].addLink(node[7], 0.30);
+		node[6].addLink(node[9], 0.30);
+		
+		//node 7
+		node[7].addLink(node[8], 0.25);
+		node[7].addLink(node[9], 0.25);
+		
+		//node 8
+		node[8].addLink(node[9], 0.25);
+		
+		//node 9 0,1,7,8 already set
+		*/
+	}
+
+}
